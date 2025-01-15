b8c3e520c9bc51057dd25a33281660f687b1f166
Harishri2002 <harishribr4@gmail.com>
2025-01-15 20:27:16 +0530

    Futurstic_Loader_Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import styles from "./App.module.css";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Hero } from "./components/Hero/Hero";
@@ -6,18 +7,46 @@ import { Experience } from "./components/Experience/Experience";
 import { Projects } from "./components/Projects/Projects";
 import { Contacts } from "./components/Contacts/Contacts";
 import { Connect } from "./components/Mail/Connect";
-function App() {
-  return <div className={styles.App}> 
-    <Navbar/>
-    <Hero/>
-    <About/>
-    <Experience/>
-    <Projects/>
-    <Connect/>
-    <Contacts/>
-    
-  </div>
 
+function Loader() {
+  return (
+    <div className={styles.loaderContainer}>
+      <div className={styles.loaderText}>Welcome to My World</div>
+      <div className={styles.spinner}></div>
+    </div>
+  );
 }
 
-export default App
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating asset loading
+    const loadAssets = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 3000)); // Simulate 3 seconds loading time
+      setIsLoading(false);
+    };
+    loadAssets();
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader /> // Show loader while loading
+      ) : (
+        <div className={styles.App}>
+          <Navbar />
+          <Hero />
+          <About />
+          <Experience />
+          <Projects />
+          <Connect />
+          <Contacts />
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
